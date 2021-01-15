be95e5146833e4fd808373863e31b300e89c3182
Matthew Easley <matteasley8791@gmail.com>
2021-01-15 00:52:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,19 +1,3 @@
-def using_concat(array1, array2)
-array1.concat(array2)
-end
-
-def using_insert(array, element)
-  new_array = array.insert(4, element)
-end
-
-def using_uniq(array)
-  array.uniq
-end
-
-def using_flatten(array)
-  array.flatten
-end
-
 def using_delete(array, "Steve")
   array.delete("Steve") 
 end 
