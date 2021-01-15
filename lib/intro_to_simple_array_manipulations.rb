1f2fb879d7cc0ff28641dc3fe205fba38cdc1b77
Matthew Easley <matteasley8791@gmail.com>
2021-01-15 00:55:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,7 @@
-def using_delete(array, steve)
-  array.delete(array, steve) 
+def using_delete(array, "Steve")
+  array.delete(array, "Steve") 
 end 
 
 def using_delete_at(array, 1)
-  array.delate_at(1)
+  array.delate_at(array, 1)
 end 
