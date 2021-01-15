6cc822abe881e876f5f88ff2d41fc3b353e0b2c4
Matthew Easley <matteasley8791@gmail.com>
2021-01-15 00:56:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,11 +13,3 @@ end
 def using_flatten(array)
   array.flatten
 end
-
-def using_delete(array, "Steve")
-  array.delete(array, "Steve") 
-end 
-
-def using_delete_at(array, 1)
-  array.delate_at(array, 1)
-end 
